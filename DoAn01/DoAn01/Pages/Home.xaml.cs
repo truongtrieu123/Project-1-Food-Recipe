d6bbd1a6b042e17e089762da7679c5bbf7929d7a
truongtrieu123 <uerittruong@gmail.com>
2020-10-22 10:42:44 +0700

    Revert "update homepage"
    This reverts commit 82b2295e0bc02515e1b9409bd67bb413f98f0cec.

--- DoAn01/DoAn01/Pages/Home.xaml.cs
+++ DoAn01/DoAn01/Pages/Home.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,128 +19,14 @@ namespace DoAn01
     /// <summary>
     /// Interaction logic for Home.xaml
     /// </summary>
-    /// 
-    public class Meal
-    {
-        public string imageMeal { get; set; }
-        public string mealName { get; set; }
-
-        public string favorite { get; set; }
-
-    }
-
-    public class MealDAO
-    {
-        
-        public static BindingList<Meal> GetAll()
-        {
-            BindingList<Meal> _list = new BindingList<Meal>()
-            {
-                new Meal()
-                {
-                    imageMeal="./../../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="white",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-                new Meal()
-                {
-                    imageMeal="./../Images/food01.jpg",
-                    mealName= "Thịt kho trứng",
-                    favorite="red",
-
-                },
-            };
-            return _list;
-        }
-    }
     public partial class Home : Page
     {
-
-        #region Attribute 
         private System.Timers.Timer _timer;
-
-
-        BindingList<Meal> _list;
-        #endregion
-
         public Home()
         {
             InitializeComponent();
             
-        }
-
-
-        public void CreateHomePage()
-        {
-            _list=MealDAO.GetAll();
-            mealListView.ItemsSource = _list;
-        }
-
-        private void HomePage_Loaded(object sender, RoutedEventArgs e)
-        {
-            var folder = AppDomain.CurrentDomain.BaseDirectory;
-
-            MessageBox.Show($"{folder}");
-            CreateHomePage();
-            
-        }
-
+        }  
+        
     }
 }
